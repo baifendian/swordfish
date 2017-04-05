50e2c37ced440806d954476077ed80d2390796f6
操经纬 <caojingwei@caojingweis-MacBook-Pro.local>
2017-04-05 20:43:12 +0800

    fix bug

--- web-server/src/test/java/com/baifendian/swordfish/webserver/api/service/ScheduleServiceTest.java
+++ web-server/src/test/java/com/baifendian/swordfish/webserver/api/service/ScheduleServiceTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2017 Baifendian Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.baifendian.swordfish.webserver.api.service;
 
 import com.baifendian.swordfish.common.consts.Constants;
@@ -34,7 +49,7 @@ import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 /**
- * Created by caojingwei on 2017/4/5.
+ * schedule service 单元测试
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = RestfulApiApplication.class)
