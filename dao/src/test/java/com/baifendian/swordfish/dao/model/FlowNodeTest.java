fc9f8fd7ab33ec6b3ca741fa853bf79012bba6d5
xiangel <xiangel1@126.com>
2017-04-06 15:50:57 +0800

    json反序列化测试案例

--- dao/src/test/java/com/baifendian/swordfish/dao/model/FlowNodeTest.java
+++ dao/src/test/java/com/baifendian/swordfish/dao/model/FlowNodeTest.java
@@ -15,14 +15,47 @@
  */
 package com.baifendian.swordfish.dao.model;
 
+import com.baifendian.swordfish.dao.utils.json.JsonUtil;
+
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FlowNodeTest {
   @Test
   public void testSetDep() throws IOException {
     FlowNode flowNode = new FlowNode();
     flowNode.setDep("[\"shelljob1\",\"shelljob2\"]");
+  }
+
+  @Test
+  public void testDeserializer(){
+    String nodeStr="{\"name\":\"shelljob1\",\"desc\":\"shell\",\"type\":\"VIRTUAL\",\"parameter\":{\"script\":\"echo shelljob1\"},\"dep\":[],\"extras\":null}";
+    FlowNode flowNode = JsonUtil.parseObject(nodeStr, FlowNode.class);
+    List<FlowNode> nodes = new ArrayList<>();
+    nodes.add(flowNode);
+    flowNode.setName("shelljob2");
+    nodes.add(flowNode);
+    String nodesStr = JsonUtil.toJsonString(nodes);
+    System.out.println(nodesStr);
+    /*
+    String nodesStr="[{\"name\":\"shelljob1\",\"desc\":\"shell\",\"type\":\"VIRTUAL\",\"parameter\":{\"script\":\"echo shelljob1\"},\"dep\":[],\"extras\":null},{\"name\":\"shelljob2\",\"desc\":\"shell\",\"type\":\"VIRTUAL\",\"parameter\":{\"script\":\"echo shelljob2\"},\"dep\":[],\"extras\":null},{\"name\":\"shelljob3\",\"desc\":\"shell\",\"type\":\"SHELL\",\"parameter\":{\"script\":\"echo shelljob3;sleep 5\"},\"dep\":[\"shelljob1\",\"shelljob2\"],\"extras\":null},{\"name\":\"shelljob4\",\"desc\":\"shelljob4\",\"type\":\"SHELL\",\"parameter\":{\"script\":\"echo shelljob4\"},\"dep\":[],\"extras\":null}]\n";
+    List<FlowNode> nodes = JsonUtil.parseObjectList(nodeStr, FlowNode.class);
+    */
+  }
+
+  @Test
+  public void testDeserializer2() throws IOException {
+    FlowNode flowNode = new FlowNode();
+    flowNode.setName("shellJob1");
+    flowNode.setType("VIRTUAL");
+    flowNode.setParameter("{\"script\":\"echo shelljob1\"}");
+    flowNode.setDep("[]");
+    String flowNodeStr = JsonUtil.toJsonString(flowNode);
+    System.out.println(flowNodeStr);
+    FlowNode flowNode1 = JsonUtil.parseObject(flowNodeStr, FlowNode.class);
+    System.out.println(flowNode1.getParameter());
   }
 }
