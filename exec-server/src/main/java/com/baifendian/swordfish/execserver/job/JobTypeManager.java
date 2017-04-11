63fa3591da84d3353646be088293fa7fc5191b52
matrix2011 <dqf2008@mail.ustc.edu.cn>
2017-04-11 16:46:31 +0800

    1.类型管理修改; 2.sql 修改注释.

--- exec-server/src/main/java/com/baifendian/swordfish/execserver/job/JobTypeManager.java
+++ exec-server/src/main/java/com/baifendian/swordfish/execserver/job/JobTypeManager.java
@@ -18,13 +18,10 @@ package com.baifendian.swordfish.execserver.job;
 import com.baifendian.swordfish.common.job.Job;
 import com.baifendian.swordfish.common.job.JobProps;
 import com.baifendian.swordfish.common.job.exception.ExecException;
-//import com.baifendian.swordfish.execserver.job.mr.MrJob;
-import com.baifendian.swordfish.execserver.adhoc.AdHocSqlJob;
 import com.baifendian.swordfish.execserver.job.hive.EtlSqlJob;
 import com.baifendian.swordfish.execserver.job.mr.MrJob;
 import com.baifendian.swordfish.execserver.job.shell.ShellJob;
 import com.baifendian.swordfish.execserver.job.spark.SparkJob;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.ResourceUtils;
@@ -32,6 +29,9 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -40,9 +40,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+//import com.baifendian.swordfish.execserver.job.mr.MrJob;
 
 /**
  * job生成工厂类
@@ -51,6 +49,13 @@ public class JobTypeManager {
 
   private static final Logger logger = LoggerFactory.getLogger(JobTypeManager.class);
 
+  private static final String MR = "MR";
+  private static final String SHELL = "SHELL";
+  private static final String SPARK = "SPARK";
+  private static final String VIRTUAL = "VIRTUAL";
+  private static final String HQL = "HQL";
+  private static final String SPARK_STREAMING = "SPARK-STREAMING";
+
   private static Map<String, JobType> jobTypeMap = new HashMap<>();
 
   static {
@@ -58,13 +63,13 @@ public class JobTypeManager {
   }
 
   private static void initBaseJobType() {
-    jobTypeMap.put("MR", new JobType(MrJob.class, false));
-    jobTypeMap.put("SHELL", new JobType(ShellJob.class, false));
-    jobTypeMap.put("SPARK", new JobType(SparkJob.class, false));
-    jobTypeMap.put("VIRTUAL", new JobType(NoopJob.class, false));
-    jobTypeMap.put("HQL", new JobType(EtlSqlJob.class, false));
+    jobTypeMap.put(MR, new JobType(MrJob.class, false));
+    jobTypeMap.put(SHELL, new JobType(ShellJob.class, false));
+    jobTypeMap.put(SPARK, new JobType(SparkJob.class, false));
+    jobTypeMap.put(VIRTUAL, new JobType(NoopJob.class, false));
+    jobTypeMap.put(HQL, new JobType(EtlSqlJob.class, false));
+    jobTypeMap.put(SPARK_STREAMING, new JobType(SparkJob.class, true));
 
-    jobTypeMap.put("SPARK-STREAMING", new JobType(SparkJob.class, true));
     addPluginJobs();
 
     printPluginJobs();
@@ -73,7 +78,7 @@ public class JobTypeManager {
   /**
    * 根据配置文件解析plugin job
    */
-  private static void addPluginJobs(){
+  private static void addPluginJobs() {
     try {
       File pluginFile = ResourceUtils.getFile("classpath:plugin_jobs.xml");
       DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -85,52 +90,57 @@ public class JobTypeManager {
         String className = doc.getElementsByTagName("className").item(i).getFirstChild().getNodeValue();
         String isLongStr = doc.getElementsByTagName("isLongJob").item(i).getFirstChild().getNodeValue();
         boolean isLong = false;
-        if(isLongStr.equalsIgnoreCase("true")){
+        if (isLongStr.equalsIgnoreCase("true")) {
           isLong = true;
         }
-        Class <? extends Job> jobClassName = (Class<? extends Job>) Class.forName(className);
-        if(jobTypeMap.containsKey(name)){
+        Class<? extends Job> jobClassName = (Class<? extends Job>) Class.forName(className);
+        if (jobTypeMap.containsKey(name)) {
           logger.error("job type name {} is exists for {} ignore", name, jobTypeMap.get(name));
         } else {
           jobTypeMap.put(name, new JobType(jobClassName, isLong));
         }
       }
     } catch (FileNotFoundException e) {
-      e.printStackTrace();
+      logger.error("Catch an exception", e);
     } catch (ParserConfigurationException e) {
-      e.printStackTrace();
+      logger.error("Catch an exception", e);
     } catch (SAXException e) {
-      e.printStackTrace();
+      logger.error("Catch an exception", e);
     } catch (IOException e) {
-      e.printStackTrace();
+      logger.error("Catch an exception", e);
     } catch (ClassNotFoundException e) {
-      e.printStackTrace();
+      logger.error("Catch an exception", e);
     }
   }
 
   public static Job newJob(String jobIdLog, String jobTypeStr, JobProps props, Logger logger) throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException {
-    Job job = null;
-    JobType jobType = jobTypeMap.get(jobTypeStr);
+    JobType jobType = jobTypeMap.get(jobTypeStr.toUpperCase());
+    Job job;
+
     if (jobType == null) {
+      logger.error("unsupport job type: {}", jobTypeStr);
       throw new ExecException("unsupport job type:" + jobTypeStr);
     } else {
       Constructor<Job> constructor = jobType.getClassName().getConstructor(String.class, JobProps.class, Logger.class);
       job = constructor.newInstance(jobIdLog, props, logger);
     }
+
     return job;
   }
 
-  public static boolean isLongJob(String jobTypeStr){
-    if(!jobTypeMap.containsKey(jobTypeStr)){
+  public static boolean isLongJob(String jobTypeStr) {
+    if (!jobTypeMap.containsKey(jobTypeStr.toUpperCase())) {
+      logger.error("unsupport job type: {}", jobTypeStr);
       throw new IllegalArgumentException("not found job type " + jobTypeStr);
     }
-    JobType jobType = jobTypeMap.get(jobTypeStr);
+
+    JobType jobType = jobTypeMap.get(jobTypeStr.toUpperCase());
     return jobType.isLong();
   }
 
-  private static void printPluginJobs(){
+  private static void printPluginJobs() {
     logger.info("active plugin jobs:");
-    for(Map.Entry entry: jobTypeMap.entrySet()){
+    for (Map.Entry entry : jobTypeMap.entrySet()) {
       logger.info("job type {} {}", entry.getKey(), entry.getValue());
     }
   }
