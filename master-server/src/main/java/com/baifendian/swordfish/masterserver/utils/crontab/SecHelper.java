8ce8b9eb0770092684b8dfd3b68c2b9786a79ad9
caojingwei <1306245946@qq.com>
2017-05-16 17:23:14 +0800

    补充注释信息

--- master-server/src/main/java/com/baifendian/swordfish/masterserver/utils/crontab/SecHelper.java
+++ master-server/src/main/java/com/baifendian/swordfish/masterserver/utils/crontab/SecHelper.java
@@ -1,10 +1,25 @@
+/*
+ * Copyright (C) 2017 Baifendian Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.baifendian.swordfish.masterserver.utils.crontab;
 
 import com.baifendian.swordfish.dao.enums.ScheduleType;
 import com.cronutils.model.Cron;
 
 /**
- * Created by caojingwei on 2017/5/15.
+ * 秒周期工具类
  */
 public class SecHelper extends CycleHelper {
 
@@ -14,7 +29,7 @@ public class SecHelper extends CycleHelper {
 
   @Override
   protected ScheduleType getCycle() {
-    //TODO 先忽律秒周期
+    //TODO 先忽略秒周期
     return null;
   }
 }
