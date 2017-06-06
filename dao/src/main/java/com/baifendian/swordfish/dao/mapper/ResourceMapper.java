f0d705497b017d8611fdfb09ec850e456f1974e4
caojingwei <1306245946@qq.com>
2017-06-06 10:48:05 +0800

    修复了webserver的一些BUG

--- dao/src/main/java/com/baifendian/swordfish/dao/mapper/ResourceMapper.java
+++ dao/src/main/java/com/baifendian/swordfish/dao/mapper/ResourceMapper.java
@@ -48,7 +48,7 @@ public interface ResourceMapper {
       @Result(property = "ownerId", column = "owner_id", javaType = int.class, jdbcType = JdbcType.INTEGER),
       @Result(property = "owner", column = "owner", javaType = String.class, jdbcType = JdbcType.VARCHAR),
       @Result(property = "projectId", column = "project_id", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
-      @Result(property = "projectName", column = "project_name", javaType = int.class, jdbcType = JdbcType.INTEGER),
+      @Result(property = "projectName", column = "project_name", javaType = String.class, jdbcType = JdbcType.VARCHAR),
       @Result(property = "createTime", column = "create_time", javaType = Timestamp.class, jdbcType = JdbcType.DATE),
       @Result(property = "modifyTime", column = "modify_time", javaType = Timestamp.class, jdbcType = JdbcType.DATE)
   })
