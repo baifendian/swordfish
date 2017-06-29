47793a3815abc4a4fef1811c5e7538f0a87ce71b
matrix2011 <dqf2008@mail.ustc.edu.cn>
2017-06-29 10:09:31 +0800

    1. 任务执行完后, 执行清理(udf, import/export 目录); 2. 自定义函数, 解决重复上传 bug.

--- exec-server/src/main/java/com/baifendian/swordfish/execserver/common/FunctionUtil.java
+++ exec-server/src/main/java/com/baifendian/swordfish/execserver/common/FunctionUtil.java
@@ -20,12 +20,6 @@ import com.baifendian.swordfish.common.hadoop.HdfsClient;
 import com.baifendian.swordfish.common.job.struct.node.common.UdfsInfo;
 import com.baifendian.swordfish.common.job.struct.resource.ResourceInfo;
 import com.baifendian.swordfish.execserver.exception.ExecException;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.text.MessageFormat;
@@ -33,28 +27,28 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
 
 public class FunctionUtil {
 
   private static final String CREATE_FUNCTION_FORMAT = "create temporary function {0} as ''{1}''";
 
-  private static String hiveUdfJarBasePath = BaseConfig.getJobHiveUdfJarBasePath();
-
   /**
    * 创建自定义函数, 即 udf
    *
-   * @param udfsInfos
-   * @param execId
-   * @param logger
-   * @param srcDir     资源目录
+   * @param srcDir 资源目录
    * @param isHdfsFile 源是否是 hdfs 文件系统
-   * @return
-   * @throws IOException
-   * @throws InterruptedException
    */
-  public static List<String> createFuncs(List<UdfsInfo> udfsInfos, int execId, Logger logger, String srcDir, boolean isHdfsFile) throws IOException, InterruptedException {
+  public static List<String> createFuncs(List<UdfsInfo> udfsInfos, int execId, Logger logger,
+      String srcDir, boolean isHdfsFile) throws IOException, InterruptedException {
+    // 得到 hive udf jar 包路径
+    String hiveUdfJarPath = BaseConfig.getJobHiveUdfJarPath(execId);
+
     // 是否定义了 udf 的基本目录
-    if (StringUtils.isEmpty(hiveUdfJarBasePath)) {
+    if (StringUtils.isEmpty(hiveUdfJarPath)) {
       logger.error("Not define hive udf jar path");
       throw new RuntimeException("Hive udf jar base path not defined ");
     }
@@ -65,16 +59,14 @@ public class FunctionUtil {
       Set<String> resources = getFuncResouces(udfsInfos);
 
       if (CollectionUtils.isNotEmpty(resources)) {
-        String uploadPath = hiveUdfJarBasePath + File.separator + execId;
-
         // adHoc 查询时直接通过复制 hdfs 上文件的方式来处理
         if (isHdfsFile) {
-          copyJars(resources, uploadPath, srcDir, logger);
+          copyJars(resources, hiveUdfJarPath, srcDir, logger);
         } else {
-          uploadUdfJars(resources, uploadPath, srcDir, logger);
+          uploadUdfJars(resources, hiveUdfJarPath, srcDir, logger);
         }
 
-        addJarSql(funcList, resources, uploadPath);
+        addJarSql(funcList, resources, hiveUdfJarPath);
       }
     }
 
@@ -84,9 +76,6 @@ public class FunctionUtil {
 
   /**
    * 获取所有函数的资源
-   *
-   * @param udfsInfos
-   * @return
    */
   private static Set<String> getFuncResouces(List<UdfsInfo> udfsInfos) {
     Set<String> resources = new HashSet<>();
@@ -104,14 +93,9 @@ public class FunctionUtil {
 
   /**
    * 上传 udf jar
-   *
-   * @param resources
-   * @param tarDir
-   * @param srcDir
-   * @throws IOException
-   * @throws InterruptedException
    */
-  private static void uploadUdfJars(Set<String> resources, String tarDir, String srcDir, Logger logger) throws IOException, InterruptedException {
+  private static void uploadUdfJars(Set<String> resources, String tarDir, String srcDir,
+      Logger logger) throws IOException, InterruptedException {
     HdfsClient hdfsClient = HdfsClient.getInstance();
 
     if (!hdfsClient.exists(tarDir)) {
@@ -119,34 +103,32 @@ public class FunctionUtil {
     }
 
     for (String res : resources) {
-      String cmd = String.format("hdfs dfs -put %s/%s %s", srcDir, res, tarDir);
+      // 如果目标不存在, 则进行拷贝
+      if (!hdfsClient.exists(String.format("%s/%s", tarDir, res))) {
+        String cmd = String.format("hdfs dfs -put %s/%s %s", srcDir, res, tarDir);
 
-      logger.debug("cmd:{}", cmd);
+        logger.debug("cmd:{}", cmd);
 
-      Process process = Runtime.getRuntime().exec(cmd);
-      int ret = process.waitFor();
-      if (ret != 0) {
-        logger.error("run cmd:" + cmd + " error");
+        Process process = Runtime.getRuntime().exec(cmd);
+        int ret = process.waitFor();
+        if (ret != 0) {
+          logger.error("run cmd:" + cmd + " error");
 
-        String msg = IOUtils.toString(process.getErrorStream(), Charset.forName("UTF-8"));
+          String msg = IOUtils.toString(process.getErrorStream(), Charset.forName("UTF-8"));
 
-        logger.error(msg);
+          logger.error(msg);
 
-        throw new ExecException(msg);
+          throw new ExecException(msg);
+        }
       }
     }
   }
 
   /**
    * 复制资源文件到目标目录
-   *
-   * @param resources
-   * @param tarDir
-   * @param srcDir
-   * @throws IOException
-   * @throws InterruptedException
    */
-  private static void copyJars(Set<String> resources, String tarDir, String srcDir, Logger logger) throws IOException, InterruptedException {
+  private static void copyJars(Set<String> resources, String tarDir, String srcDir, Logger logger)
+      throws IOException, InterruptedException {
     HdfsClient hdfsClient = HdfsClient.getInstance();
 
     if (!hdfsClient.exists(tarDir)) {
@@ -154,17 +136,18 @@ public class FunctionUtil {
     }
 
     for (String res : resources) {
-      logger.debug("create symlink {}/{} -> {}/{}", tarDir, res, srcDir, res);
-      hdfsClient.copy(String.format("%s/%s", srcDir, res), String.format("%s/%s", tarDir, res), false, true);
+      // 如果目标不存在才进行拷贝
+      if (!hdfsClient.exists(String.format("%s/%s", tarDir, res))) {
+        logger.debug("create symlink {}/{} -> {}/{}", tarDir, res, srcDir, res);
+        hdfsClient
+            .copy(String.format("%s/%s", srcDir, res), String.format("%s/%s", tarDir, res), false,
+                true);
+      }
     }
   }
 
   /**
    * 添加 jar
-   *
-   * @param sqls
-   * @param resources
-   * @param uploadPath
    */
   private static void addJarSql(List<String> sqls, Set<String> resources, String uploadPath) {
     if (!uploadPath.startsWith("hdfs:")) {
@@ -178,13 +161,11 @@ public class FunctionUtil {
 
   /**
    * 添加临时函数
-   *
-   * @param sqls
-   * @param udfsInfos
    */
   private static void addTempFuncSql(List<String> sqls, List<UdfsInfo> udfsInfos) {
     for (UdfsInfo udfsInfo : udfsInfos) {
-      sqls.add(MessageFormat.format(CREATE_FUNCTION_FORMAT, udfsInfo.getFunc(), udfsInfo.getClassName()));
+      sqls.add(MessageFormat
+          .format(CREATE_FUNCTION_FORMAT, udfsInfo.getFunc(), udfsInfo.getClassName()));
     }
   }
 }
